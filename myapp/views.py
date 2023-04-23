54fef4ddcc43c2bddab64f07c8fe564f60051ef6
jayanthmahadesh <jayanthmahadesh777@gmail.com>
2023-04-23 11:47:35 +0530

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def homepage(request):
-    return render(request,'homepage.html')
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey=ca86407e7be949aba091db60b69ea959'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    if category:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&country=in&apiKey=ca86407e7be949aba091db60b69ea959'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    
+    context = {
+        'articles' : articles
+    }
+
+    return render(request,'homepage.html',context)
